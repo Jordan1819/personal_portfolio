a5ad6d9809c8728676390db6d1fae2fb673ccde1
Jordan <jmanwaite@gmail.com>
2025-05-18 07:39:13 -0600

    Frontend

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,37 @@
+import { useState } from 'react';
+import '../index.css'
+
 export default function Navbar() {
-    return (
-      <nav>
-        <div class="nav-right">
-        <a href="index.html">⌂ Home</a>
-        <a href="#">🖳 Projects</a>
-        <a href="#">▤ Resume</a>
-        <a href="https://github.com/jordan1819" target="_blank" rel="noopener noreferrer" className="nav-link githubLink">
-          <span className="githubIconWrapper">
-            <img src="../githubIcon.png" alt="GitHub" className="githubIcon githubIcon--white" />
-            <img src="../githubIconGray.png" alt="GitHub Dark" className="githubIcon githubIcon--dark" />
-          </span> 
-          <span className="githubText">GitHub</span>
-        </a>
-        <a href="#">✉ Contact</a>
-        <a href="#">✎ Blog</a>
-        </div>
-      </nav>
-    );
-  }
-  
+
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
+  return (
+    <nav>
+
+      {/* Hamburger Menu */}
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+
+      {/* Full nav menu */}
+      <div className={`nav-right ${menuOpen ? 'open' : ''}`}>
+      <a href="index.html">⌂ Home</a>
+      <a href="#">🖳 Projects</a>
+      <a href="#">▤ Resume</a>
+      <a href="https://github.com/jordan1819" target="_blank" rel="noopener noreferrer" className="nav-link githubLink">
+        <span className="githubIconWrapper">
+          <img src="../githubIcon.png" alt="GitHub" className="githubIcon githubIcon--white" />
+          <img src="../githubIconGray.png" alt="GitHub Dark" className="githubIcon githubIcon--dark" />
+        </span> 
+        <span className="githubText">GitHub</span>
+      </a>
+      <a href="#">✉ Contact</a>
+      {/* <a href="#">✎ Blog</a> */}
+      </div>
+
+    </nav>
+  );
+}
