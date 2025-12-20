6b8ff680fa0e3b5a54b8b11bb87da6b814768ed8
Jordan <jmanwaite@gmail.com>
2025-12-20 11:40:01 -0700

    Updated base & navbar styles slightly

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -9,6 +9,17 @@ export default function ContactCard({
     linkDescription
     }) {
     return (
-        
+        <div className="contact-card">
+            <h3 className="contact-card-title">{title}</h3>
+            <p>{description}</p>
+            <a
+                href={link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="contact-card-link"
+            >
+                {linkDescription}
+            </a>
+        </div>
     );
 }
